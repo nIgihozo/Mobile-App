64642c4cf8687babd689ff8c889ccd617a3f0a5f
nIgihozo <n.igihozo@alustudent.com>
2026-07-01 22:42:57 +0200

    Solving some authentication matters on the firebase side

--- config/authFirebase.ts
+++ config/authFirebase.ts
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCl85IWnvM61UVwEX6KK6qH0cAsIQIOWkU",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: "movie-app-2326f.firebaseapp.com",
   projectId: "movie-app-2326f",
   storageBucket: "movie-app-2326f.firebasestorage.app",
   messagingSenderId: "1027546557566",
-  appId: "1:1027546557566:web:dbf1d7ee3f57cf343a3c2f",
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
   measurementId: "G-WTM4Q9JJ2F"
 };
 
